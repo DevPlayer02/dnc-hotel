721948ffbdb50fc555844d096bd0a26fcfbbe9f1
DevPlayer02 <contato.giovanicf@gmail.com>
2025-06-17 22:07:10 -0300

    Feat: Adding register route

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { User } from 'generated/prisma';
+import { Role, User } from 'generated/prisma';
 import { AuthLoginDTO } from './domain/dto/authLogin.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { UserService } from '../users/user.services';
+import { CreateUserDTO } from '../users/domain/dto/createUser.dto';
+import { AuthRegisterDTO } from './domain/dto/authRegister.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly prisma: PrismaService,
+    private readonly userService: UserService,
   ) {}
 
   async generateJwtToken(user: User) {
@@ -24,14 +28,25 @@ export class AuthService {
   }
 
   async login({ email, password }: AuthLoginDTO) {
-    const user = await this.prisma.user.findUnique({
-      where: { email },
-    });
+    const user = await this.userService.findByEmail(email);
 
     if (!user || (await bcrypt.compare(password, user.password))) {
       throw new UnauthorizedException('Email or password is incorrect');
     }
 
     return this.generateJwtToken(user);
+  }
+
+  async register(body: AuthRegisterDTO) {
+    const newUser: CreateUserDTO = {
+      name: body.name!,
+      email: body.email!,
+      password: body.password!,
+      role: body.role ?? Role.USER,
+    };
+
+    const user = await this.userService.create(newUser);
+
+    return await this.generateJwtToken(user);
   }
 }
