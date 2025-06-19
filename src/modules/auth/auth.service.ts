91809af767c501e5a1770f33f7305088f8a4a5c6
DevPlayer02 <contato.giovanicf@gmail.com>
2025-06-19 15:29:58 -0300

    Feat: Adding reset password and debugging findByEmail

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -7,6 +7,7 @@ import * as bcrypt from 'bcrypt';
 import { UserService } from '../users/user.services';
 import { CreateUserDTO } from '../users/domain/dto/createUser.dto';
 import { AuthRegisterDTO } from './domain/dto/authRegister.dto';
+import { AuthResetPasswordDTO } from './domain/dto/authResetPassword.dto';
 
 @Injectable()
 export class AuthService {
@@ -30,8 +31,18 @@ export class AuthService {
   async login({ email, password }: AuthLoginDTO) {
     const user = await this.userService.findByEmail(email);
 
-    if (!user || (await bcrypt.compare(password, user.password))) {
-      throw new UnauthorizedException('Email or password is incorrect');
+    if (!user) {
+      throw new UnauthorizedException('Email ou senha incorretos');
+    }
+
+    if (!password || !user.password) {
+      throw new UnauthorizedException('Credenciais incompletas');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      throw new UnauthorizedException('Email ou senha incorretos');
     }
 
     return this.generateJwtToken(user);
@@ -46,6 +57,30 @@ export class AuthService {
     };
 
     const user = await this.userService.create(newUser);
+
+    return this.generateJwtToken(user);
+  }
+
+  async resetPassword({ token, password }: AuthResetPasswordDTO) {
+    type JwtPayload = {
+      sub: string;
+      name?: string;
+      iat?: number;
+      exp?: number;
+      aud?: string;
+      iss?: string;
+    };
+    const decoded: JwtPayload = await this.jwtService.verifyAsync<JwtPayload>(
+      token,
+      {
+        audience: 'users',
+        issuer: 'dnc_hotel',
+      },
+    );
+
+    const user = await this.userService.update(Number(decoded.sub), {
+      password,
+    });
 
     return await this.generateJwtToken(user);
   }
