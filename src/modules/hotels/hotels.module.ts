cff4732af589a5515d2d99fef5d46cac52a1d224
DevPlayer02 <contato.giovanicf@gmail.com>
2025-08-11 19:54:25 -0300

    feat: Adding flow of hotel image

--- src/modules/hotels/hotels.module.ts
+++ src/modules/hotels/hotels.module.ts
@@ -12,9 +12,26 @@ import { FindByNameHotelsService } from './services/findByNameHotel.service';
 import { FindByOwnerHotelsService } from './services/findByOwnerHotel.service';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../users/user.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { UploadHotelImageService } from './services/uploadHotelImage.service';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads-hotel',
+        filename: (req, file, cb) => {
+          const filename = `${uuidv4()}${file.originalname}`;
+          return cb(null, filename);
+        },
+      }),
+    }),
+  ],
   controllers: [HotelsController],
   providers: [
     CreateHotelsService,
@@ -24,6 +41,7 @@ import { UserModule } from '../users/user.module';
     FindByOwnerHotelsService,
     RemoveHotelService,
     UpdateHotelService,
+    UploadHotelImageService,
     {
       provide: HOTEL_REPOSITORY_TOKEN,
       useClass: HotelsRepositories,
