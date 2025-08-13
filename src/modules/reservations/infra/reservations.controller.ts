f7f6df70f7a7578056a68ddabbbf51b3d3f0572d
DevPlayer02 <contato.giovanicf@gmail.com>
2025-08-13 12:53:24 -0300

    feat: Adding update status reservation

--- src/modules/reservations/infra/reservations.controller.ts
+++ src/modules/reservations/infra/reservations.controller.ts
@@ -1,23 +1,18 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, UseGuards } from '@nestjs/common';
 import { CreateReservationDto } from '../domain/dto/createReservation.dto';
 import { CreateReservationsService } from '../services/createReservations.service';
 import { User } from 'src/shared/decorators/user.decorator';
 import { AuthGuard } from 'src/shared/guards/auth.guard';
 import { FindAllReservationsService } from '../services/findAllReservation.service';
-import { FindByIdReservationsService } from '../services/findByIdReservation.service copy';
+import { FindByIdReservationsService } from '../services/findByIdReservation.service';
 import { ParamId } from 'src/shared/decorators/paramId.decorator';
-import { FindByUserReservationsService } from '../services/findByUserReservation.service copy 2';
+import { FindByUserReservationsService } from '../services/findByUserReservation.service';
+import { ReservationStatus, Role } from 'generated/prisma';
+import { updateStatusReservationsService } from '../services/UpdateStatusReservation.service';
+import { RoleGuard } from 'src/shared/guards/role.guard';
+import { Roles } from 'src/shared/decorators/roles.decorators';
 
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
 @Controller('reservations')
 export class ReservationsController {
   constructor(
@@ -25,8 +20,10 @@ export class ReservationsController {
     private readonly findAllReservationsService: FindAllReservationsService,
     private readonly findByIdReservationsService: FindByIdReservationsService,
     private readonly findByUserReservationsService: FindByUserReservationsService,
+    private readonly updateStatusReservationsService: updateStatusReservationsService,
   ) {}
 
+  @Roles(Role.USER)
   @Post()
   create(@User('id') id: number, @Body() body: CreateReservationDto) {
     return this.createReservationsService.execute(id, body);
@@ -47,13 +44,11 @@ export class ReservationsController {
     return this.findByIdReservationsService.execute(id);
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateReservationDto: UpdateReservationDto) {
-  //   return this.reservationsService.update(+id, updateReservationDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.reservationsService.remove(+id);
-  // }
+  @Patch(':id')
+  updateStatus(
+    @ParamId() id: number,
+    @Body('status') status: ReservationStatus,
+  ) {
+    return this.updateStatusReservationsService.execute(id, status);
+  }
 }
